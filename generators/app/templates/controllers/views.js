f78e067de53f8e2a20a267f1fd99ad6ad76ed9ff
Pavel Bakharev <baharevpavel@gmail.com>
2018-01-09 12:02:32 +0700

    Updated dependencies, minor fix for views.js

--- generators/app/templates/controllers/views.js
+++ generators/app/templates/controllers/views.js
@@ -4,14 +4,14 @@ const parse = require('co-body');
 const fs = require('fs');
 
 function read(src) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(src, {'encoding': 'utf8'}, (err, data) => {
       if(err) return reject(err);
       resolve(data);
     });
   });
 };
 
-module.exports.home = async function home() {
-  this.body = await read('./views/index.html');
+module.exports.home = async (ctx) => {
+  ctx.body = await read('./views/index.html');
 };
